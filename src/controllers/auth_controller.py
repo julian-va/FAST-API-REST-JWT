75657018d298d70f88c1329240746a133122389a
julian-va <julian.villegas2014@gmail.com>
2022-01-16 20:21:19 -0500

    correcciones

--- src/controllers/auth_controller.py
+++ src/controllers/auth_controller.py
@@ -1,6 +1,6 @@
 from os import getenv
 from fastapi import APIRouter, status, Body, Depends, Header
-from src.models.user import User_login
+from src.models.user import User_base, User_login
 from src.services.auth_service import Auth_service
 from fastapi.responses import JSONResponse
 from src.libs.jwt_functions import Jwt_methods
@@ -14,8 +14,10 @@ oauth_2passwor = OAuth2PasswordBearer("/api/v1/auth/login")
 @routes_auth.post(path="/login", summary="Login a User", tags=["Auth"])
 async def login(user: User_login = Body(...), service: Auth_service = Depends(Auth_service)):
     try:
-        result = await service.login_verification(user.user_hashed_password, user.user_email)
+        result: tuple = await service.login_verification(user.user_hashed_password, user.user_email)
         if result:
+            user: User_base = result[1]
+            user.creation_date = None
             token = Jwt_methods.write_token(
                 user.dict(), int(getenv("TOKEN_DURATION")))
             return JSONResponse(content={"token": f"{token}"}, status_code=status.HTTP_202_ACCEPTED)
@@ -27,6 +29,7 @@ async def login(user: User_login = Body(...), service: Auth_service = Depends(Au
 
 @routes_auth.post(path="/token/verify", summary="verify token", tags=["Auth"])
 async def token_verify(Authorization: str = Header(default=None)):
-    token = Authorization.split(" ")[1]
-    return Jwt_methods.token_validate(token, outpul=True)
-    print(token)
+    if Authorization != None:
+        token = Authorization.split(" ")[1]
+        return Jwt_methods.token_validate(token, outpul=True)
+    return JSONResponse(content={"message": "Not token authorization"}, status_code=status.HTTP_404_NOT_FOUND)
--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,16 +1,19 @@
+
 from src.libs.utilities import Utilities
 from src.databases.config_db import get_db, Session
 from src.entity_model.entitys import User_entity
 from fastapi import Depends
+from src.mappers.user_mapper import User_mapper
 
 
 class Auth_service():
     """docstring for Auth_service."""
 
-    def __init__(self, db: Session = Depends(get_db)):
+    def __init__(self, db: Session = Depends(get_db), maper_user: User_mapper = Depends(User_mapper)):
         self._db = db
+        self._user_mapper = maper_user
 
-    async def login_verification(self, password: str, user_email: str) -> bool:
+    async def login_verification(self, password: str, user_email: str):
         try:
             verification: bool = False
             result: User_entity = self._db.query(User_entity).filter(
@@ -19,6 +22,6 @@ class Auth_service():
             if result:
                 verification = await Utilities.compare_password(password, result.user_hashed_password)
 
-            return verification
+            return verification, await self._user_mapper.user_entity_to_user_pydantic(result)
         except Exception as e:
             raise e
